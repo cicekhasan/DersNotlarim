aa229e3f13d4265a6c8c97d10eff9e2c5668d80c
Hasan Çiçek <hasan.cicek@yandex.com.tr>
2020-05-22 23:44:33 +0300

    dosya yükleme örneği eklendi.

--- ornekler/dosyaYukleme/sonuc.php
+++ ornekler/dosyaYukleme/sonuc.php
@@ -5,15 +5,15 @@
   }else{
     if (is_uploaded_file($_FILES['dosya']['tmp_name'])) {
       $gecerliUzantilar = ['image/jpg', 'image/png', 'image/jpeg'];
-      $gecerliBoyut     = (1024 * 1024 * 3);
+      $gecerliBoyut     = (1024 * 1024 * 4);
       $uzanti           = $_FILES['dosya']['type'];
       $uzantiParcala    = explode('/', $uzanti);
       $zaman            = explode('-', date('g-m-Y'));
       $saat             = explode(':', date('H:i:s'));
       $vakit            = $zaman[0].$zaman[1].$zaman[2].$saat[0].$saat[1].$saat[2];
       //$isim             = uniqid(); // Kullanılabilir...
-      $isim             = rand(1000, 9999);
-      $isim            .= $vakit;
+      $isim             = rand(100000, 999999);
+      $isim            .= '-'.$vakit;
       $isim            .= ".".$uzantiParcala[1];
 
       if (in_array($uzanti, $gecerliUzantilar)) {
@@ -27,7 +27,7 @@
             echo "Dosya yüklenemedi...";
           }
         }else{
-          echo "En fazla 3MB dosya yükleyebilirsiniz!";
+          echo "En fazla 4MB dosya yükleyebilirsiniz!";
         }
       }else{
         echo "Dosya sadece jpg, jpeg ve png formatında olabilir!";
