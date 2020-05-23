4d119bd087ae23995a11c7288b896257577e067b
Hasan Çiçek <hasan.cicek@yandex.com.tr>
2020-05-23 21:36:58 +0300

    Güncellendi.

--- ornekler/cokluDosyaYukleme/sonuc.php
+++ ornekler/cokluDosyaYukleme/sonuc.php
@@ -1,13 +1,15 @@
 <?php 
+//print_r($_FILES);
+
 function cokluDosyaYukle($dosyalar){
   $sonuc = [];
 
   // Hataları kontrol et...
-  foreach ($dosyalar['error'] as $index => $error) {
+  foreach ($dosyalar['error'] as $key => $error) {
     if ($error == 4) {
       $sonuc['hata'] = 'Lütfen bir dosya seçiniz!';
     }elseif ($error != 0) {
-      $sonuc['hata'][] = 'Dosya yüklenirken bir sorun oluştu #'.$dosyalar['name'][$index];
+      $sonuc['hata'][] = 'Dosya yüklenirken bir sorun oluştu #'.$dosyalar['name'][$key];
     }
   }
 
@@ -15,26 +17,27 @@ function cokluDosyaYukle($dosyalar){
   if (!isset($sonuc['hata'])) {
     // Dosya uzantılarını kontrol et...
     $gecerliUzantilar = ['image/jpg', 'image/png', 'image/jpeg'];
-    foreach ($dosyalar['type'] as $index => $type) {
+    foreach ($dosyalar['type'] as $key => $type) {
       if (!in_array($type, $gecerliUzantilar)) {
-        $sonuc['hata'][] = 'Dosya geçersiz formatta #'.$dosyalar['name'][$index];
+        $sonuc['hata'][] = 'Dosya geçersiz formatta #'.$dosyalar['name'][$key];
       }
     }
     // Boyutu kontrol edelim...
     if (!isset($sonuc['hata'])) {
-      $maxBoyut = (1024 * 1024);
-      foreach ($dosyalar['size'] as $index => $size) {
+      $maxBoyut = (1024 * 1024 * 4);
+      foreach ($dosyalar['size'] as $key => $size) {
         if ($size > $maxBoyut) {
-          $sonuc['hata'][] = 'Dosya beklenenden fazla büyük boyutta #'.$dosyalar['name'][$index];
+          $sonuc['hata'][] = 'Dosya beklenenden fazla büyük boyutta #'.$dosyalar['name'][$key];
         }
       }
       // Dosyaları yükle...
       if (!isset($sonuc['hata'])) {
-        foreach ($dosyalar['tmp_name'] as $index => $tmp) {
-          $dosyaAdi = $dosyalar['name'][$index];
+        foreach ($dosyalar['tmp_name'] as $key => $tmp) {
+          $dosyaAdi = $dosyalar['name'][$key];
           $yukle = move_uploaded_file($tmp, 'uploads/'.$dosyaAdi);
           if ($yukle) {
             $sonuc['dosya'][] = 'uploads/'.$dosyaAdi;
+            header('Location: index.php');
           }else{
             $sonuc['hata'][] = 'Dosya yüklenemedi #'.$dosyaAdi;
           }
